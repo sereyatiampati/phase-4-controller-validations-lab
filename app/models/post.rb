81a7f49777212c2804a40bc9e800ade489f918d9
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-15 10:46:40 +0300

    custom validation and active record model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validate :must_be_fiction_or_nonfiction
+
+    #Custom validation
+    def must_be_fiction_or_nonfiction
+      unless (category == "Fiction" || category == "Non-Fiction")
+        errors.add(:category, "is not included in the list")
+        end
+    end
 end
